808fd5be23863a814d2326cc57139df0684d1ae7
vitoriastavis <vitoriastavis@gmail.com>
2025-10-29 16:24:42 -0300

    network

--- get_metabolites.R
+++ get_metabolites.R
@@ -4,36 +4,29 @@ get_metabolites <- function(analysis_targets) {
   
   load("data/target_prediction/stpnovo/target_metabolites_stp.RData")
   stp_mappings <- read.csv("data/target_prediction/stpnovo/stp_mappings.csv")
+  target_metabolites_stp <- target_metabolites_stp[names(target_metabolites_stp) %in% analysis_targets]
   
   sea_result <- read_xlsx("data/target_prediction/sea/sea-combined.xlsx")
   sea_mappings <- read.csv("data/target_prediction/sea/sea_mappings.csv")
   colnames(sea_result) <- c("Query_ID", "Target_ID", "Affinity_Threshold", "P_Value",
                             "Max_Tc", "Cut_Sum", "Z_Score", "Name", "Description", "Query_Smiles")
   sea_result <- sea_result[grepl("_HUMAN$", sea_result$Target_ID), ]
+  sea_result <- sea_result[sea_result$Name %in% analysis_targets, ]
   target_metabolites_sea <- split(sea_result$Query_ID, sea_result$Name)
   
-  chembl_mappings <- read.csv("data/target_prediction/chemblpred/chemblpred_mappings.csv")
-  chembl_mappings <- chembl_mappings[!is.na(chembl_mappings$HGNC_ID),]
-  load("data/target_prediction/chemblpred/target_metabolites_chemblpred.RData")
-  target_metabolites_chemblpred_chemblid <- target_metabolites_chemblpred
+  multitask_mappings <- read.csv("data/target_prediction/multitask/multitask_mappings.csv")
+  multitask_mappings <- multitask_mappings[!is.na(multitask_mappings$HGNC_ID),]
+  load("data/target_prediction/multitask/target_metabolites_multitask.RData")
+  target_metabolites_multitask <- target_metabolites_multitask[names(target_metabolites_multitask) %in% analysis_targets]
   
-  target_metabolites_chemblpred <- list()
-  for (target in names(target_metabolites_chemblpred_chemblid)){
-    hgnc_id <- chembl_mappings[chembl_mappings$Query == target, "HGNC_ID"]
-    
-    if (length(hgnc_id) == 1){
-      target_metabolites_chemblpred[[hgnc_id]] <- target_metabolites_chemblpred_chemblid[target][[1]]
-    }
-    else if (length(hgnc_id) == 2){
-      for (id in hgnc_id){
-        target_metabolites_chemblpred[[id]] <- target_metabolites_chemblpred_chemblid[target][[1]]  
-      }
-    }
-  }
+  ppb_mappings <- read.csv("data/target_prediction/ppb2/ppb_mappings.csv")
+  ppb_mappings <- ppb_mappings[!is.na(ppb_mappings$HGNC_ID),]
+  load("data/target_prediction/ppb2/target_metabolites_ppb.RData")
+  target_metabolites_ppb <- target_metabolites_ppb[names(target_metabolites_ppb) %in% analysis_targets]
   
   targets_metabolites <- list()
   for (target in analysis_targets){
-    print(target)
+    # print(target)
     all_metabolites <- c()
     
     # Get SEA metabolites
@@ -42,9 +35,8 @@ get_metabolites <- function(analysis_targets) {
 
     all_metabolites <- c(sea_metabolites,
                          target_metabolites_stp[[target]],
-                         target_metabolites_chemblpred[[target]])
-    
-    print(length(all_metabolites))
+                         target_metabolites_multitask[[target]],
+                         target_metabolites_ppb[[target]])
     
     original_queries <- sea_mappings[!is.na(sea_mappings$HGNC_ID) & sea_mappings$HGNC_ID == target, ]
     for (query in original_queries$Query) {
@@ -53,18 +45,32 @@ get_metabolites <- function(analysis_targets) {
       all_metabolites <- c(all_metabolites, metabolites)
     }
     
+    # print(length(all_metabolites))
+
     original_queries <- stp_mappings[!is.na(sea_mappings$HGNC_ID) & stp_mappings$HGNC_ID == target, ]
     for (query in original_queries$Query) {
       all_metabolites <- c(all_metabolites, target_metabolites_stp[[query]])
     }
     
-    original_queries <- chembl_mappings[!is.na(chembl_mappings$HGNC_ID) & chembl_mappings$HGNC_ID == target, ]
+    # print(length(all_metabolites))
+
+    original_queries <- multitask_mappings[!is.na(multitask_mappings$HGNC_ID) & multitask_mappings$HGNC_ID == target, ]
     for (query in original_queries$Query) {
-      all_metabolites <- c(all_metabolites, target_metabolites_chemblpred[[query]])
+      all_metabolites <- c(all_metabolites, target_metabolites_multitask[[query]])
     }
-    print(length(all_metabolites))
+    
+    # print(length(all_metabolites))
+    
+    original_queries <- ppb_mappings[!is.na(ppb_mappings$HGNC_ID) & ppb_mappings$HGNC_ID == target, ]
+    for (query in original_queries$Query) {
+      all_metabolites <- c(all_metabolites, target_metabolites_ppb[[query]])
+    }
+    
+    # print(length(all_metabolites))
     
     targets_metabolites[[target]] <- unique(all_metabolites)
+    
+    # print(length(all_metabolites))
   }
   
   return(targets_metabolites)
